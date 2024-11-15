35bda505cf55af0eb954e26effda38873fc34f6b
Zoofear <misterz0312@gmail.com>
2024-11-15 18:34:24 -0500

    Started Header File for IntToDate

---
+++ IntToDate/IntToDate.h
@@ -0,0 +1,24 @@
+//
+#pragma once
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class IntToDate
+{
+
+public:
+
+	//Constructor
+
+private:
+
+	
+	int inputdate;
+
+	
+	string outputdate;
+
+};
